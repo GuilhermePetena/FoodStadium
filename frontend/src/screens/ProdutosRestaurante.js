63ecb2fc1cfb1bf19fb9465bde5179b6323c711f
Tadao Suzuki <48482811+TadaoFS@users.noreply.github.com>
2020-07-27 19:37:51 -0300

    Add files via upload

---
+++ frontend/src/screens/ProdutosRestaurante.js
@@ -0,0 +1,17 @@
+import Navbar from '../components/Navbar'
+import React,{Component, useState} from 'react'
+import {StyleSheet, View,ImageBackground} from 'react-native'
+import { TextInput ,Text,Button,Searchbar } from 'react-native-paper';
+
+
+export default class ProdutosRestaurante extends Component {
+    render(){
+
+        return(
+            <>
+                
+            </> 
+        )
+       
+    }
+}
