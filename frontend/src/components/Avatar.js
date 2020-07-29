93dcf6031038049a78a6358c55ecf0e922d775a1
Samantha Ogawa <42716919+samyogawa@users.noreply.github.com>
2020-07-29 18:44:20 -0300

    Add files via upload

---
+++ frontend/src/components/Avatar.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { BottomNavigation, Text,Avatar } from 'react-native-paper';
+
+
+    const AvatarCliente = () => {
+        return(
+            <Avatar.Text size={80} label="SO" />        
+        )
+    }
+        
+       
+ 
+        
+  
+  export default AvatarCliente;
