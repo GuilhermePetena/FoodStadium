4e74599089c05eb866b889be27a9cd6bc84106f7
Samantha Ogawa <42716919+samyogawa@users.noreply.github.com>
2020-08-03 18:41:11 -0300

    Add files via upload

---
+++ frontend/Cliente/Perfil.js
@@ -0,0 +1,102 @@
+
+import React,{Component, useState} from 'react'
+import {StyleSheet, View,FlatList, TouchableOpacity} from 'react-native'
+import {Text,Avatar,Divider  } from 'react-native-paper';
+import Navbar from '../components/Navbar'
+import AvatarCliente from '../components/Avatar'
+
+
+export default class Perfil extends Component {
+    
+    render(){
+        const renderItem = ({ item }) => (
+            <Item
+                item={item}
+                onPress={() => <Navbar pagina='produtos'></Navbar>}
+            />
+          );
+          
+          
+          const Item = ({ item, onPress}) => (
+            <TouchableOpacity onPress={onPress} style={styles.item}>
+                <Text style={styles.itemTxt}>{item.title}</Text>
+            </TouchableOpacity> 
+          );
+
+         
+          
+        return(
+            <>
+                
+                <View style={styles.viewInicio}>
+                    <View style={styles.titulo}>      
+                        <AvatarCliente></AvatarCliente>
+                        <Text style={styles.txtPerfil}>PERFIL</Text>
+                    </View> 
+                    <View style={styles.corpo}>    
+                        <Text style={styles.txtMeio}>Editar Perfil</Text>    
+                        <Divider />      
+                        <Text style={styles.txtMeio} >Configurações</Text>
+                        <Divider />
+                        <Text style={styles.txtMeio} >Cartões</Text>
+                    </View>
+                           
+                </View>
+            </> 
+        )
+           
+    }
+}
+
+const styles = StyleSheet.create({
+  viewInicio:{
+    flex:1,
+    alignItems:'center'
+  },
+
+  navbar:{
+      flex:1
+  },
+  itemTxt:{
+      fontSize:20
+  },
+  item:{
+     marginTop:20
+  },
+  
+  txtMeio:{
+    fontSize:20,
+    fontWeight:'bold',
+    marginTop:20,
+  },
+  txtPerfil:{
+    fontSize:30,
+    fontWeight:'bold',
+    marginTop:20
+  },
+  corpo:{
+    marginTop:10,
+    flex:2,
+    alignItems:'center'
+  },
+  titulo:{
+      flex:1,
+      marginTop:40,
+      alignItems:'center'
+  },
+  avatar:{
+
+      alignItems:'center'
+  }
+    
+})
+
+
+
+
+  
+  
+  
+  
+  
+    
