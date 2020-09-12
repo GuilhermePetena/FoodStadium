3b2cbf3f3dde0bc5c61844fccb677148cadfdd9a
Guilherme Maciel <guilherme.petena@gmail.com>
2020-09-12 17:07:34 -0300

    Update PedidoRepository.java

--- backend/src/main/java/br/com/java/foodstadiumapi/repository/PedidoRepository.java
+++ backend/src/main/java/br/com/java/foodstadiumapi/repository/PedidoRepository.java
@@ -11,4 +11,6 @@ public interface PedidoRepository extends JpaRepository<Pedido, Long> {
     List<Pedido> findAllbyStatusAndTipoEntrega(String status, String tipo);
     @Query(value = "SELECT * FROM PEDIDO AS P JOIN STATUS AS S ON P.STATUS_ID = S.ID JOIN TIPO_ENTREGA AS TE ON P.TIPO_ENTREGA_ID = TE.ID WHERE S.NOME = ? AND TE.NOME = ?",nativeQuery = true)
     List<Pedido> findAllbyStatusAndTipoEntregaAndEntregador(String status, String tipo, Long id);
+    @Query(value = "SELECT * FROM PEDIDO AS P JOIN STATUS AS S ON P.STATUS_ID = S.ID JOIN CLIENTE_LOCAL_SETOR_BLOCO AS CLSB ON P.CLIENTE_LOCAL_SETOR_BLOCO_ID = CLSB.ID WHERE S.NOME = ? AND CLSB.CLIENTE_ID = ?",nativeQuery = true)
+    List<Pedido> findAllByStatus_NomeAndClienteLocalSetorBloco_Cliente(String status, Long id);
 }
