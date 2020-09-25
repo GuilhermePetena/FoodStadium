0ab4e3dfb9bf1b416428d5aa25512c80955ad489
Guilherme Maciel <guilherme.petena@gmail.com>
2020-09-25 19:48:12 -0300

    Update PedidoRepository.java

--- backend/src/main/java/br/com/java/foodstadiumapi/repository/PedidoRepository.java
+++ backend/src/main/java/br/com/java/foodstadiumapi/repository/PedidoRepository.java
@@ -9,6 +9,8 @@ import java.util.List;
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
     @Query(value = "SELECT * FROM PEDIDO AS P JOIN STATUS AS S ON P.STATUS_ID = S.ID JOIN TIPO_ENTREGA AS TE ON P.TIPO_ENTREGA_ID = TE.ID WHERE S.NOME = ? AND TE.NOME = ?",nativeQuery = true)
     List<Pedido> findAllbyStatusAndTipoEntrega(String status, String tipo);
+      @Query(value = "SELECT * FROM PEDIDO AS P JOIN STATUS AS S ON P.STATUS_ID = S.ID JOIN TIPO_ENTREGA AS TE ON P.TIPO_ENTREGA_ID = TE.ID WHERE S.NOME = ? AND TE.NOME = ? AND P.ENTREGADOR_LOCAL_SETOR_ID = ? AND P.ID = ?",nativeQuery = true)
+    List<Pedido> findAllbyStatusAndTipoEntregaAndEntregadorLocalSetorId(String status, String tipo, Long id, Long idPedido);
     @Query(value = "SELECT * FROM PEDIDO AS P JOIN STATUS AS S ON P.STATUS_ID = S.ID JOIN TIPO_ENTREGA AS TE ON P.TIPO_ENTREGA_ID = TE.ID WHERE S.NOME = ? AND TE.NOME = ?",nativeQuery = true)
     List<Pedido> findAllbyStatusAndTipoEntregaAndEntregador(String status, String tipo, Long id);
     @Query(value = "SELECT * FROM PEDIDO AS P JOIN STATUS AS S ON P.STATUS_ID = S.ID JOIN CLIENTE_LOCAL_SETOR_BLOCO AS CLSB ON P.CLIENTE_LOCAL_SETOR_BLOCO_ID = CLSB.ID WHERE S.NOME = ? AND CLSB.CLIENTE_ID = ?",nativeQuery = true)
